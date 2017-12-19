97789d5276dd3d3a85a9065e03713d6956d3b916
Dimitar Trifonov <dimitar.trifonov@mentormate.com>
2017-12-19 11:59:32 +0200

    fixed iot url

--- device-app/check-cert.sh
+++ device-app/check-cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl s_client -connect <YourUniqueClientIdentifier>.<YourAWSRegion>.amazonaws.com:8883 -CAfile ./certs/root-CA.crt -cert ./certs/certificate.pem.crt -key ./certs/private.pem.key
+openssl s_client -connect <YourUniqueIotClientIdentifier>.iot.<YourAWSRegion>.amazonaws.com:8883 -CAfile ./certs/root-CA.crt -cert ./certs/certificate.pem.crt -key ./certs/private.pem.key
